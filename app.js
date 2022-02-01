2c5a73b94ab35282a3fa3cc34ce8006401dc552a
Katriin Poolakese <katriin.poolaese@khk.ee>
2022-02-01 19:49:07 +0200

    Refactored populated data srtucture items to list

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ const ItemCtrl = (function(){
 
 // UI controller
 const UICtrl = (function(){
+	// UI selectors
+	const UISelectors = {
+		itemList: '#item-list'
+	}
 	return {
 		populateItemList: function(items){
 			// Create html content
@@ -48,7 +52,7 @@ const UICtrl = (function(){
 			});
 
 			// Insert list items
-			document.querySelector("#item-list").innerHTML = html;
+			document.querySelector(UISelectors.itemList).innerHTML = html;
 		}
 	}
 })();
